0aa33628f87154a4c6378dca4a4c46631fd3ec40
asp <asp@aptech.com>
2022-07-29 19:51:51 +0500

    E-Project

---
+++ E-Project/eproject/JS/Packages.js
@@ -0,0 +1,12 @@
+function btn1(){
+    alert("You have successfully subscribed to Freeware. Thankyou!")
+}
+function btn2(){
+    alert("You have successfully subscribed to Beginner. Thankyou!")
+}
+function btn3(){
+    alert("You have successfully subscribed to Intermediate. Thankyou!")
+}
+function btn4(){
+    alert("You have successfully subscribed to Professional. Thankyou!")
+}
